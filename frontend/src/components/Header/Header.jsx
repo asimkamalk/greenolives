38f99960367ce790ba20b4773c73e599c0ec5c96
Asim kamal <asimkamalk@gmail.com>
2025-08-10 20:13:35 +0500

    choose word added to the Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
             <div className='header-overlay'></div>
             <div className='header-contents'>
                 <h2>Order your <span className="highlight">favourite</span> food here</h2>
-                <p>from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
                 <button onClick={() => {
                   const menuSection = document.getElementById('explore-menu');
                   if (menuSection) {
