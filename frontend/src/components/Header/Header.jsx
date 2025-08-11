4ce8712be1d78444fa9d7aefe31561b68f79f0f5
Asim kamal <asimkamalk@gmail.com>
2025-08-12 03:32:31 +0500

    Remove problematic orange image completely and replace with beautiful deep green/black gradient

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,30 +1,7 @@
 import React from 'react'
 import './Header.css'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 
 const Header = () => {
-    // Minimal slider settings
-    const settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        fade: true,
-        pauseOnHover: true
-    }
-
-    // Use only olive-themed images - but let's try a different approach
-    const sliderImages = [
-        '/olive_hero.jpg'
-        // We'll use CSS to filter out any orange colors
-    ]
-
     const handleViewMenuClick = () => {
         console.log('Button clicked!') // Debug log
         const menuSection = document.getElementById('explore-menu');
@@ -35,19 +12,8 @@ const Header = () => {
 
     return (
         <div className='header'>
-            {/* Background Slider with CSS filters to remove orange */}
-            <div className='slider-container'>
-                <Slider {...settings}>
-                    {sliderImages.map((image, index) => (
-                        <div key={index} className="slider-item">
-                            <div 
-                                className="slider-image" 
-                                style={{ backgroundImage: `url(${image})` }}
-                            />
-                        </div>
-                    ))}
-                </Slider>
-            </div>
+            {/* Pure CSS background - no more orange images */}
+            {/* Using deep green/black gradient as requested */}
             
             {/* Dark overlay */}
             <div className='header-overlay' />
