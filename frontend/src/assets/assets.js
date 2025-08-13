ca9283179a2d2eac8966a5b4e16aeae07c9a6f87
Asim kamal <asimkamalk@gmail.com>
2025-08-13 17:10:23 +0500

    category images changed

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,47 +2,39 @@ import basket_icon from './basket_icon.png'
 import logo from '/logo.jpeg'
 import header_img from './olive_hero.jpg';
 import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
-import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
 
-import food_1 from './food_1.png'
-import food_2 from './food_2.png'
-import food_3 from './food_3.png'
-import food_4 from './food_4.png'
-import food_5 from './food_5.png'
-import food_6 from './food_6.png'
-import food_7 from './food_7.png'
-import food_8 from './food_8.png'
-import food_9 from './food_9.png'
-import food_10 from './food_10.png'
-import food_11 from './food_11.png'
-import food_12 from './food_12.png'
-import food_13 from './food_13.png'
-import food_14 from './food_14.png'
-import food_15 from './food_15.png'
-import food_16 from './food_16.png'
-import food_17 from './food_17.png'
-import food_18 from './food_18.png'
-import food_19 from './food_19.png'
-import food_20 from './food_20.png'
-import food_21 from './food_21.png'
-import food_22 from './food_22.png'
-import food_23 from './food_23.png'
-import food_24 from './food_24.png'
-import food_25 from './food_25.png'
-import food_26 from './food_26.png'
-import food_27 from './food_27.png'
-import food_28 from './food_28.png'
-import food_29 from './food_29.png'
-import food_30 from './food_30.png'
-import food_31 from './food_31.png'
-import food_32 from './food_32.png'
+// Menu category images - 29 unique images
+import appetizer from './appetizer.png'
+import kids_special from './kids_special.png'
+import salad_zone from './salad_zone.png'
+import greens_classic from './greens_classic.png'
+import soup_corner from './soup_corner.png'
+import greens_special from './greens_special.png'
+import tea_lover from './tea_lover.png'
+import sandwich from './sandwich.png'
+import cold_coffee from './cold_coffee.png'
+import burger_crowd from './burger_crowd.png'
+import iced_coffee from './iced_coffee.png'
+import panini_corner from './panini_corner.png'
+import special_drinks from './special_drinks.png'
+import coladas from './coladas.png'
+import pizza_house from './pizza_house.png'
+import steak_house from './steak_house.png'
+import pasta_la_vistas from './pasta_la_vistas.png'
+import around_world from './around_world.png'
+import rice_corner from './rice_corner.png'
+import chinese_corner from './chinese_corner.png'
+import bbq_corner from './bbq_corner.png'
+import pakistani_corner from './pakistani_corner.png'
+import sea_food from './sea_food.png'
+import greens_platters from './greens_platters.png'
+import naan from './naan.png'
+import smoothies from './smoothies.png'
+import margaritas from './margaritas.png'
+import regular_drinks from './regular_drinks.png'
+import icecream_dessert from './icecream_dessert.png'
+
+
 
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
@@ -87,35 +79,35 @@ export const assets = {
 }
 
 export const menu_list = [
-    { menu_name: "Appetizer", menu_image: menu_1 },
-    { menu_name: "Kids Special", menu_image: menu_2 },
-    { menu_name: "Salad Zone", menu_image: menu_3 },
-    { menu_name: "Green's Classic", menu_image: menu_4 },
-    { menu_name: "Soup Corner", menu_image: menu_5 },
-    { menu_name: "Green's Special", menu_image: menu_6 },
-    { menu_name: "Tea Lover", menu_image: menu_7 },
-    { menu_name: "Sandwich", menu_image: menu_8 },
-    { menu_name: "Cold Coffee", menu_image: menu_1 },
-    { menu_name: "Burger Crowd", menu_image: menu_2 },
-    { menu_name: "Iced Coffee", menu_image: menu_3 },
-    { menu_name: "Panini Corner", menu_image: menu_4 },
-    { menu_name: "Special Drinks", menu_image: menu_5 },
-    { menu_name: "Colada's", menu_image: menu_6 },
-    { menu_name: "Pizza House", menu_image: menu_7 },
-    { menu_name: "Steak House", menu_image: menu_8 },
-    { menu_name: "Pasta La Vistas", menu_image: menu_1 },
-    { menu_name: "Around The World", menu_image: menu_2 },
-    { menu_name: "Rice Corner", menu_image: menu_3 },
-    { menu_name: "Chinese Corner", menu_image: menu_4 },
-    { menu_name: "BBQ Corner", menu_image: menu_5 },
-    { menu_name: "Pakistani Corner", menu_image: menu_6 },
-    { menu_name: "Sea Food", menu_image: menu_7 },
-    { menu_name: "Green's Platters", menu_image: menu_8 },
-    { menu_name: "Naan", menu_image: menu_1 },
-    { menu_name: "Smoothies", menu_image: menu_2 },
-    { menu_name: "Margaritas's", menu_image: menu_3 },
-    { menu_name: "Regular Drinks", menu_image: menu_4 },
-    { menu_name: "Icecream / Dessert", menu_image: menu_5 },
+    { menu_name: "Appetizer", menu_image: appetizer },
+    { menu_name: "Kids Special", menu_image: kids_special },
+    { menu_name: "Salad Zone", menu_image: salad_zone },
+    { menu_name: "Green's Classic", menu_image: greens_classic },
+    { menu_name: "Soup Corner", menu_image: soup_corner },
+    { menu_name: "Green's Special", menu_image: greens_special },
+    { menu_name: "Tea Lover", menu_image: tea_lover },
+    { menu_name: "Sandwich", menu_image: sandwich },
+    { menu_name: "Cold Coffee", menu_image: cold_coffee },
+    { menu_name: "Burger Crowd", menu_image: burger_crowd },
+    { menu_name: "Iced Coffee", menu_image: iced_coffee },
+    { menu_name: "Panini Corner", menu_image: panini_corner },
+    { menu_name: "Special Drinks", menu_image: special_drinks },
+    { menu_name: "Colada's", menu_image: coladas },
+    { menu_name: "Pizza House", menu_image: pizza_house },
+    { menu_name: "Steak House", menu_image: steak_house },
+    { menu_name: "Pasta La Vistas", menu_image: pasta_la_vistas },
+    { menu_name: "Around The World", menu_image: around_world },
+    { menu_name: "Rice Corner", menu_image: rice_corner },
+    { menu_name: "Chinese Corner", menu_image: chinese_corner },
+    { menu_name: "BBQ Corner", menu_image: bbq_corner },
+    { menu_name: "Pakistani Corner", menu_image: pakistani_corner },
+    { menu_name: "Sea Food", menu_image: sea_food },
+    { menu_name: "Green's Platters", menu_image: greens_platters },
+    { menu_name: "Naan", menu_image: naan },
+    { menu_name: "Smoothies", menu_image: smoothies },
+    { menu_name: "Margaritas's", menu_image: margaritas },
+    { menu_name: "Regular Drinks", menu_image: regular_drinks },
+    { menu_name: "Icecream / Dessert", menu_image: icecream_dessert },
 ];
 
 export const food_list =
